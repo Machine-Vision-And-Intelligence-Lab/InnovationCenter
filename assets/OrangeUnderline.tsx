13fb89e8c18ca7fc2edd5557bbe7b6b753e21f1d
sisantaChhatoi <sisantachhatoi@gmail.com>
2025-07-11 00:44:26 +0530

    Revert "Extracting the div providing orange underline to a separate component."
    This reverts commit 2a7efcf9f03970a80de290ea69376bf630fea6b4.

--- assets/OrangeUnderline.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const OrangeUnderline = () => {
-    return (
-            <div  className="text-2xl  mt-9.5  sm:mt-9.5 w-3/12 h-1 sm:w-1/12 rounded-2xl bg-[#F46721]"  ></div>
-    );
-};
-
-export default OrangeUnderline;
