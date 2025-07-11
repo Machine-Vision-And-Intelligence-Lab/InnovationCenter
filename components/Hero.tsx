3cad3c2c0209f9b00eec7cd0aabb23a21add371f
sisantaChhatoi <sisantachhatoi@gmail.com>
2025-07-11 17:51:47 +0530

    Making the hero section fit the screen.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Hero = () => {
     return (
-        <div className="grid grid-cols-1 md:mb-6 md:grid-cols-2 w-full place-items-center gap-4 h-[70vh] bg-gradient-to-r from-gray-400 via-gray-500  to-gray-600 ">
+        <div className="grid grid-cols-1 md:mb-6 md:grid-cols-2 w-full place-items-center gap-4 h-[90vh] bg-gradient-to-r from-gray-400 via-gray-500  to-gray-600 ">
             <div >
                 <div className="sm:text-4xl text-3xl text-white sm:tracking-wide"><span className="text-[#F46721]">i</span> HUB</div>
                 <div className="sm:text-4xl text-3xl text-white sm:tracking-wide">NIT <span className="text-[#F46721]">JAMSHEDPUR</span></div>
