fa09c277a02b453fa35ced061a7d666965c4fabb
sisantaChhatoi <sisantachhatoi@gmail.com>
2025-07-11 13:10:10 +0530

    Adding the card component.

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,11 @@ import DirectorInfo from "@/components/DirectorInfo";
 
 export default function Home() {
   return (
-    // <div className="min-h-screen flex items-center justify-center">
-    //   <h1 className="font-bold text-7xl">Home Page</h1>
+
         <div className="container-fluid page ml-1">
         <DirectorInfo/>
-
         </div>
-    // </div>
+
 
   );
 }
